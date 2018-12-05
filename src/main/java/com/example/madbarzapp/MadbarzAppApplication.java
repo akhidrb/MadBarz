77ca89dacb787cce6b2324b1ecb6eba11ba3c535
Khidr <akhidrb@gmail.com>
2018-12-05 23:24:28 +0200

    first commit

---
+++ src/main/java/com/example/madbarzapp/MadbarzAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.madbarzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadbarzAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadbarzAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/madbarzapp/MadbarzAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.madbarzapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MadbarzAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
